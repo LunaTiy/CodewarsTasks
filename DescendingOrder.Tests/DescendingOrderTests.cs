49f8f505d8efb08b164489a641ad85f6d5da92e2
LunaTiy <savushkin-daniil-99@yandex.ru>
2022-04-23 00:07:36 +0300

    Add descending order task
    Task description is in Program.cs file

---
+++ DescendingOrder.Tests/DescendingOrderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DescendingOrder;
+using Xunit;
+
+namespace DescendingOrder.Tests
+{
+	public class DescendingOrderTests
+	{
+		[Fact]
+		public void DescendingOrderTest01()
+		{
+			Assert.Equal(43210, Kata.DescendingOrder(23140));
+		}
+
+		[Fact]
+		public void DescendingOrderTest02()
+		{
+			Assert.Equal(886553311, Kata.DescendingOrder(518158633));
+		}
+
+		[Fact]
+		public void DescendingOrderTest03()
+		{
+			Assert.Equal(0, Kata.DescendingOrder(-100));
+		}
+	}
+}
---
+++ DescendingOrder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+    Your task is to make a function that can take any non-negative integer as an argument and
+    return it with its digits in descending order.
+    Essentially, rearrange the digits to create the highest possible number.
+
+    Examples:
+    Input: 42145 Output: 54421
+    Input: 145263 Output: 654321
+    Input: 123456789 Output: 987654321
+ */
+
+namespace DescendingOrder
+{
+    public static class Kata
+    {
+        public static int DescendingOrder(int num)
+        {
+            if (num <= 0)
+                return 0;
+
+            List<int> numbers = Divide(num);
+            numbers.Sort();
+
+            return AssemblyToNumber(numbers);
+        }
+
+        private static List<int> Divide(int num)
+        {
+            List<int> numbers = new List<int>();
+
+            while (num != 0)
+            {
+                numbers.Add(num % 10);
+                num /= 10;
+            }
+
+            return numbers;
+        }
+
+        private static int AssemblyToNumber(List<int> numbers)
+		{
+            int number = 0;
+
+            for(int i = 0; i < numbers.Count; i++)
+			{
+                number +=  numbers[i] * (int) Math.Pow(10, i);
+            }
+
+            return number;
+		}
+    }
+    class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
