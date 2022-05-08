671848005897143c84ce8fd5d22c0c0e54431c02
LunaTiy <savushkin-daniil-99@yandex.ru>
2022-05-08 16:40:57 +0300

    Rework project struct

--- 7 kyu/DescendingOrder/Kata.cs
+++ 7 kyu/DescendingOrder/Kata.cs
@@ -1,21 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-/*
-    Your task is to make a function that can take any non-negative integer as an argument and
-    return it with its digits in descending order.
-    Essentially, rearrange the digits to create the highest possible number.
-
-    Examples:
-    Input: 42145 Output: 54421
-    Input: 145263 Output: 654321
-    Input: 123456789 Output: 987654321
- */
-
 namespace DescendingOrder
 {
-    public static class Kata
-    {
+	public class Kata
+	{
         public static int DescendingOrder(int num)
         {
             if (num <= 0)
@@ -41,21 +30,15 @@ namespace DescendingOrder
         }
 
         private static int AssemblyToNumber(List<int> numbers)
-		{
+        {
             int number = 0;
 
-            for(int i = 0; i < numbers.Count; i++)
-			{
-                number +=  numbers[i] * (int) Math.Pow(10, i);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                number += numbers[i] * (int)Math.Pow(10, i);
             }
 
             return number;
-		}
+        }
     }
-    class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
 }
